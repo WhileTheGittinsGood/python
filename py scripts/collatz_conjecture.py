bed7dcdc9e5efffa3457420acaad9099a436bfb8
Scott Kosinski <exist.k100@gmail.com>
2019-11-15 16:15:22 -0600

    fixed typo in director

---
+++ py scripts/collatz_conjecture.py
@@ -0,0 +1,51 @@
+#   Collatz conjecture script
+
+#   Takes a number (n) and:
+
+#           if it is even divides by two
+#           if it is odd multiplies by 3 and adds 1
+
+#   Counts how many steps taken to reach a final outcome of 1
+
+#   Returns the value of the counter
+
+from time import sleep
+from IPython.display import clear_output as clear
+
+def collatz(number):
+
+    clear()
+    n = number
+    c = 0
+    
+    def output(c,n):
+        
+        print(f'\nStarting point: {number}\n')
+        print(f'\tStep {c}: {n}')
+        
+        if n == 1:
+            print(f'\nCompleted in: {c} steps')
+            print(f'Time wasted: {c} seconds')
+            print(f'\nMission complete')
+        else:
+            print(f'\nCompleted in: -- steps')
+            print(f'Time wasted: {c} seconds')
+        
+        sleep(1)
+        clear(wait=True)
+
+    while n > 1:
+        
+        if n % 2 == 0:
+            n = n / 2
+            c += 1
+            output(c,n)
+        
+        else:
+            n = (n * 3) + 1
+            c += 1
+            output(c,n)
+    
+    output(c,n)
+    
+collatz(int(input('Enter a starting value for the Collatz Conjecture:')))
