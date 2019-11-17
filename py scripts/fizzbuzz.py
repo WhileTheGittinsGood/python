0ef26ba37466d68bbcb271bdc651275f403d92ea
Scott Kosinski <exist.k100@gmail.com>
2019-11-16 22:56:37 -0600

    finished fizzbuzz

--- py scripts/fizzbuzz.py
+++ py scripts/fizzbuzz.py
@@ -1,1 +1,29 @@
 # FizzBuzz
+
+def fizzbuzz(num):
+
+    n = num
+
+    while n > 0:
+
+        if n % 3 == 0 and n % 5 == 0:
+
+            print('fizzbuzz')
+            n -= 1
+
+        elif n % 3 == 0:
+
+            print('fizz')
+            n -= 1
+
+        elif n % 5:
+
+            print('buzz')
+            n -= 1
+
+        else:
+
+            print(n)
+            n -= 1
+
+fizzbuzz(int(input('enter a number for fizzbuzz to start with:')))
